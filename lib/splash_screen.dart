95714ec5abc47946d414fd5cd80b40cd78c630cb
Ahtsham0715 <ahtsham50743@gmail.com>
2022-09-29 12:06:25 +0500

    splash page added

---
+++ lib/splash_screen.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:secure_optimizer/dashboard.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  BuildContext? get context => null;
+
+  @override
+  void initState() {
+    // super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context!).pushReplacement(MaterialPageRoute(
+          builder: (BuildContext context) => const DashboardPage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset(
+        'assets/images/icon.png',
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+}
