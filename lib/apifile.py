34ec40648c01606d1b637e6fdd230481b80a00de
Ahtsham0715 <ahtsham50743@gmail.com>
2022-10-01 11:32:48 +0500

    fixes

--- lib/apifile.py
+++ lib/apifile.py
@@ -10,13 +10,13 @@ app = Flask(__name__)
 @app.route('/cpu_percent')
 def cpu_percent():
     json_file = {}
-    json_file['query'] = psutil.cpu_percent(interval=1)
+    json_file['cpu_data'] = psutil.cpu_percent(interval=1)
     return jsonify(json_file)
 
 @app.route('/ram_usage')
 def ram_usage():
     json_file = {}
-    json_file['query'] = psutil.virtual_memory().percent
+    json_file['ram_data'] = psutil.virtual_memory().percent
     return jsonify(json_file)
 
 
